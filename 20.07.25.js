9ec1dbbb4a03bfaebd776ef092ae5fa666734559
ᴅᴀɴɪɪʟ ꜱʜᴛᴇʀᴋᴇʟ <daniilshterkel@MAC-SHT.local>
2025-07-20 14:11:25 +0500

    Второй день ?

---
+++ 20.07.25.js
@@ -0,0 +1,23 @@
+let firstName = "Казакова"
+const age = 17
+var isStudent = false
+var hobby = undefined
+
+/*
+Let - это переменная, которую можно повторно объявлять и область видимости - функция.  
+Const - это переменная, которую можно объявить один раз. Подходит для постоянных значений
+Var - это переменная, которую можно изменить, однако область видимости - блок {}
+*/
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof hobby);
+
+
+let name = "Dashunya"
+let study = "medicine"
+
+console.log("In 2025 year " + name + "graduated in " + study);
+console.log(`In 2025 year, ${name} graduated in ${study}`);
+
