322d1153f06d8c8c369365656850cea6382b8f03
ᴅᴀɴɪɪʟ ꜱʜᴛᴇʀᴋᴇʟ <daniilshterkel@MAC-SHT.local>
2025-07-23 23:22:52 +0500

    Пятый день ?

---
+++ 23.07.25.js
@@ -0,0 +1,7 @@
+user = {
+    name: "Frontend-developer",
+    info: "Frontend-dev - is developer, who used language for front in, example, website or app.",
+    skills: ['HTML','Css', 'JavaScript']
+}
+
+console.log(user(skills))
