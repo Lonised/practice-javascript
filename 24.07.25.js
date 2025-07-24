162cbee2b5cfd5a94457576761f714b960210d7a
ᴅᴀɴɪɪʟ ꜱʜᴛᴇʀᴋᴇʟ <daniilshterkel@MAC-SHT.local>
2025-07-24 23:27:53 +0500

    Шестой день ?

---
+++ 24.07.25.js
@@ -0,0 +1,12 @@
+//Функции 
+function greet(name) {
+    console.log("Привет, " + name);
+}
+greet("Дашуня");
+
+
+
+function info(name, discipline) {
+    console.log(name, discipline);
+}
+info("High-Programming", "Javascript")
