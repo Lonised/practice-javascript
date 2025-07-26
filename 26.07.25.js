79f7c405464e28032afd6ad5744ce099e0fef5e7
ᴅᴀɴɪɪʟ ꜱʜᴛᴇʀᴋᴇʟ <daniilshterkel@MAC-SHT.local>
2025-07-26 13:23:16 +0500

    Восьмой день

---
+++ 26.07.25.js
@@ -0,0 +1,12 @@
+function mathSum(a, b) {
+    console.log(a + b);
+}
+function mathMin(a, b) {
+    console.log(a - b);
+}
+function Math(mathSum, mathMin) {
+    console.log(mathSum + mathMin);
+    
+}
+
+
