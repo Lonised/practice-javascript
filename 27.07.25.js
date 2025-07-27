09da5dab5f5dbf7632f3801040e598925b4099b8
ᴅᴀɴɪɪʟ ꜱʜᴛᴇʀᴋᴇʟ <daniilshterkel@MAC-SHT.local>
2025-07-27 14:40:58 +0500

    Девятый день ?

---
+++ 27.07.25.js
@@ -0,0 +1,54 @@
+function sayHello() {
+    console.log("Привет, мир!");
+    
+}
+
+sayHello()
+
+function sum(a,b) {
+    console.log(a + b);
+    
+}
+
+sum(1,2)
+
+function isEven(number) {
+    if (number % 2 == 0) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+    
+}
+
+isEven(3)
+
+function greet(name) {
+    console.log("Привет,", name);
+    
+}
+
+greet("Меня зовут ...")
+
+function maxOfThree(a, b, c) {
+    if (a >= b && a >= c) {
+        console.log(a);
+    } else if (b >= a && b >= c) {
+        console.log(b);
+    } else {
+        console.log(c);
+    }
+}
+
+maxOfThree(3,1,5)
+
+function factorial(n) {
+    let result = 1
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    console.log(result);
+    
+}
+
+factorial(5)
